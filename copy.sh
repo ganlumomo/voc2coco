478d8fb912f50408f6c3d844271bef9e8465efd3
Lu Gan <ganlu@umich.edu>
2022-08-30 16:37:21 -0700

    added copy script

---
+++ copy.sh
@@ -0,0 +1,4 @@
+cp outputs/rgb-train-coco.json ../images_rgb_train/coco.json
+cp outputs/rgb-val-coco.json ../images_rgb_val/coco.json
+cp outputs/thermal-train-coco.json ../images_thermal_train/coco.json
+cp outputs/thermal-val-coco.json ../images_thermal_val/coco.json
--- run.sh
+++ run.sh
@@ -5,16 +5,16 @@ python voc2coco.py --ann_dir ../Annotations \
     --img_type rgb \
     --ext xml
 python voc2coco.py --ann_dir ../Annotations \
-    --ann_ids ../align_train.txt \
-    --labels flir-labels.txt \
-    --output outputs/thermal-train-coco.json \
-    --img_type thermal \
-    --ext xml
-python voc2coco.py --ann_dir ../Annotations \
     --ann_ids ../align_validation.txt \
     --labels flir-labels.txt \
     --output outputs/rgb-val-coco.json \
     --img_type rgb \
+    --ext xml
+python voc2coco.py --ann_dir ../Annotations \
+    --ann_ids ../align_train.txt \
+    --labels flir-labels.txt \
+    --output outputs/thermal-train-coco.json \
+    --img_type thermal \
     --ext xml
 python voc2coco.py --ann_dir ../Annotations \
     --ann_ids ../align_validation.txt \
