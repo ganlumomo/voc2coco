8ad5e17528027d3396347d7df475b975a35e5d64
Lu Gan <ganlu@umich.edu>
2022-08-30 21:22:47 -0700

    generate both flir and coco annotations

--- copy.sh
+++ copy.sh
@@ -2,3 +2,7 @@ cp outputs/rgb-train-coco.json ../images_rgb_train/coco.json
 cp outputs/rgb-val-coco.json ../images_rgb_val/coco.json
 cp outputs/thermal-train-coco.json ../images_thermal_train/coco.json
 cp outputs/thermal-val-coco.json ../images_thermal_val/coco.json
+cp outputs/rgb-train-flir.json ../images_rgb_train/flir.json
+cp outputs/rgb-val-flir.json ../images_rgb_val/flir.json
+cp outputs/thermal-train-flir.json ../images_thermal_train/flir.json
+cp outputs/thermal-val-flir.json ../images_thermal_val/flir.json
--- run.sh
+++ run.sh
@@ -1,24 +1,24 @@
 python voc2coco.py --ann_dir ../Annotations \
     --ann_ids ../align_train.txt \
     --labels flir-labels.txt \
-    --output outputs/rgb-train-coco.json \
+    --output outputs/rgb-train-flir.json \
     --img_type rgb \
     --ext xml
 python voc2coco.py --ann_dir ../Annotations \
     --ann_ids ../align_validation.txt \
     --labels flir-labels.txt \
-    --output outputs/rgb-val-coco.json \
+    --output outputs/rgb-val-flir.json \
     --img_type rgb \
     --ext xml
 python voc2coco.py --ann_dir ../Annotations \
     --ann_ids ../align_train.txt \
     --labels flir-labels.txt \
-    --output outputs/thermal-train-coco.json \
+    --output outputs/thermal-train-flir.json \
     --img_type thermal \
     --ext xml
 python voc2coco.py --ann_dir ../Annotations \
     --ann_ids ../align_validation.txt \
     --labels flir-labels.txt \
-    --output outputs/thermal-val-coco.json \
+    --output outputs/thermal-val-flir.json \
     --img_type thermal \
     --ext xml
