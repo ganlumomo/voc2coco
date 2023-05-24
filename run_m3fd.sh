e3cb43266efb8d2e69240d45fd00b81302e3d078
Lu Gan <ganlu@umich.edu>
2023-05-23 21:10:54 -0700

    re-generated json files for m3fd

--- run_m3fd.sh
+++ run_m3fd.sh
@@ -9,6 +9,11 @@ python voc2coco_m3fd.py --ann_dir ../../../m3fd/Annotation \
     --output outputs/m3fd-val.json \
     --ext xml
 python voc2coco_m3fd.py --ann_dir ../../../m3fd/Annotation \
+    --ann_ids ../../../m3fd/meta/test.txt \
+    --labels m3fd-labels.txt \
+    --output outputs/m3fd-test.json \
+    --ext xml
+python voc2coco_m3fd.py --ann_dir ../../../m3fd/Annotation \
     --ann_ids ../../../m3fd/meta/challenge_train.txt \
     --labels m3fd-labels.txt \
     --output outputs/m3fd-challenge-train.json \
@@ -17,6 +22,11 @@ python voc2coco_m3fd.py --ann_dir ../../../m3fd/Annotation \
     --ann_ids ../../../m3fd/meta/challenge_val.txt \
     --labels m3fd-labels.txt \
     --output outputs/m3fd-challenge-val.json \
+    --ext xml
+python voc2coco_m3fd.py --ann_dir ../../../m3fd/Annotation \
+    --ann_ids ../../../m3fd/meta/challenge_test.txt \
+    --labels m3fd-labels.txt \
+    --output outputs/m3fd-challenge-test.json \
     --ext xml
 python voc2coco_m3fd.py --ann_dir ../../../m3fd/Annotation \
     --ann_ids ../../../m3fd/meta/daytime_train.txt \
@@ -29,14 +39,24 @@ python voc2coco_m3fd.py --ann_dir ../../../m3fd/Annotation \
     --output outputs/m3fd-daytime-val.json \
     --ext xml
 python voc2coco_m3fd.py --ann_dir ../../../m3fd/Annotation \
+    --ann_ids ../../../m3fd/meta/daytime_test.txt \
+    --labels m3fd-labels.txt \
+    --output outputs/m3fd-daytime-test.json \
+    --ext xml
+python voc2coco_m3fd.py --ann_dir ../../../m3fd/Annotation \
     --ann_ids ../../../m3fd/meta/night_train.txt \
     --labels m3fd-labels.txt \
-    --output outputs/m3fd-night-trin.json \
+    --output outputs/m3fd-night-train.json \
     --ext xml
 python voc2coco_m3fd.py --ann_dir ../../../m3fd/Annotation \
     --ann_ids ../../../m3fd/meta/night_val.txt \
     --labels m3fd-labels.txt \
     --output outputs/m3fd-night-val.json \
+    --ext xml
+python voc2coco_m3fd.py --ann_dir ../../../m3fd/Annotation \
+    --ann_ids ../../../m3fd/meta/night_test.txt \
+    --labels m3fd-labels.txt \
+    --output outputs/m3fd-night-test.json \
     --ext xml
 python voc2coco_m3fd.py --ann_dir ../../../m3fd/Annotation \
     --ann_ids ../../../m3fd/meta/overcast_train.txt \
@@ -47,4 +67,9 @@ python voc2coco_m3fd.py --ann_dir ../../../m3fd/Annotation \
     --ann_ids ../../../m3fd/meta/overcast_val.txt \
     --labels m3fd-labels.txt \
     --output outputs/m3fd-overcast-val.json \
+    --ext xml
+python voc2coco_m3fd.py --ann_dir ../../../m3fd/Annotation \
+    --ann_ids ../../../m3fd/meta/overcast_test.txt \
+    --labels m3fd-labels.txt \
+    --output outputs/m3fd-overcast-test.json \
     --ext xml
